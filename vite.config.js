d6b02159c92814b873afbf9cb1e4d8d958e780e0
naimulislamsaikat <nzsaikat06@gmail.com>
2026-09-07 14:16:21 +0600

    error fixing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
 // https://vite.dev/config/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Inforisen_Services_Portfolio/',
+  base: '/Inforisen_Services_Portfolio/'
 })
